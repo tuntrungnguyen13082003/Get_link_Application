9dec25931bd1ccb1dec8875188daeb97d3583475
Tuann <tuntrungnguyen@gmail.com>
2025-12-20 09:53:16 +0700

    231231

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,28 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 // Import các trang
-// LƯU Ý: Kiểm tra kỹ tên thư mục của bạn là 'Page' hay 'pages' nhé!
-import AdminPage from './pages/AdminPage'; 
-import SolarPage from './pages/SolarPage';
-import SuCoPage from './pages/SuCoPage'; 
+import AdminPage from './Page/AdminPage';
+import SolarPage from './Page/SolarPage';
+import SuCoPage from './Page/SuCoPage'; // Khi nào làm xong file SuCoPage thì mở comment này ra
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Mặc định vào Admin */}
         <Route path="/" element={<Navigate to="/admin" replace />} />
-        <Route path="/admin" element={<AdminPage />} />
-        <Route path="/solar" element={<SolarPage />} />
-        <Route path="/su-co" element={<SuCoPage />} />
-        {/* -------------------------------- */}
 
-        <Route path="*" element={<div className="p-10 text-center">404 - Trang không tồn tại</div>} />
+        {/* Trang Admin: /admin */}
+        <Route path="/admin" element={<AdminPage />} />
+
+        {/* Trang Solar: /solar */}
+        <Route path="/solar" element={<SolarPage />} />
+
+        {/* Trang Sự Cố: /su-co */}
+        <Route path="/su-co" element={<SuCoPage />} />
+
+        {/* Trang lỗi 404 */}
+        <Route path="*" element={<div className="p-10 text-center font-bold">404 - Trang không tồn tại</div>} />
       </Routes>
     </BrowserRouter>
   );
