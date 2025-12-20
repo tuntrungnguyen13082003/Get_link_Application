06b278e63f188b3117d92278958090dc8e59b3a6
Tuann <tuntrungnguyen@gmail.com>
2025-12-20 10:23:56 +0700

    111

--- src/Page/AdminPage.jsx
+++ src/Page/AdminPage.jsx
@@ -109,18 +109,18 @@ const AdminPage = () => {
             <div className="w-16 h-16 bg-slate-700 rounded-full flex items-center justify-center mx-auto mb-4">
                <Lock size={32} className="text-blue-400" />
             </div>
-            <h1 className="text-2xl font-bold uppercase tracking-wider">Đăng Nhập Admin</h1>
+            <h1 className="text-2xl font-bold uppercase tracking-wider">Get Link</h1>
           </div>
           <form onSubmit={handleLogin} className="p-8 space-y-6">
             <div>
-              <label className="block text-sm font-bold text-slate-700 mb-2">Tài khoản</label>
-              <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="w-full pl-4 py-3 bg-slate-50 border border-slate-200 rounded-xl outline-none" placeholder="Nhập tài khoản"/>
+              <label className="block text-sm font-bold text-slate-700 mb-2">User</label>
+              <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="w-full pl-4 py-3 bg-slate-50 border border-slate-200 rounded-xl outline-none" placeholder="Enter Account"/>
             </div>
             <div>
-              <label className="block text-sm font-bold text-slate-700 mb-2">Mật khẩu</label>
-              <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="w-full pl-4 py-3 bg-slate-50 border border-slate-200 rounded-xl outline-none" placeholder="Nhập mật khẩu"/>
+              <label className="block text-sm font-bold text-slate-700 mb-2">Password</label>
+              <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="w-full pl-4 py-3 bg-slate-50 border border-slate-200 rounded-xl outline-none" placeholder="Enter Password"/>
             </div>
-            <button type="submit" className="w-full bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 rounded-xl shadow-lg flex items-center justify-center gap-2"><LogIn size={20}/> ĐĂNG NHẬP</button>
+            <button type="submit" className="w-full bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 rounded-xl shadow-lg flex items-center justify-center gap-2"><LogIn size={20}/> SIGN IN</button>
           </form>
         </div>
       </div>
