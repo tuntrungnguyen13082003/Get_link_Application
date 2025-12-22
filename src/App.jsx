9170b9cc01158e36534f634cd2c616ca85cbf2c5
Tuann <tuntrungnguyen@gmail.com>
2025-12-22 17:33:20 +0700

    333

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 // Import các trang
 import AdminPage from './Page/AdminPage';
@@ -8,7 +8,7 @@ import NotFoundPage from './Page/NotFoundPage';
 
 const App = () => {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/admin" replace />} />
         <Route path="/admin" element={<AdminPage />} />
@@ -18,7 +18,7 @@ const App = () => {
 
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 };
 export default App;
