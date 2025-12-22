e34c426cbba60b4f2e207c53983998ddc2346513
Tuann <tuntrungnguyen@gmail.com>
2025-12-22 17:24:16 +0700

    456

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // <--- THÊM DÒNG NÀY (Quan trọng: dấu chấm và dấu gạch chéo)
 })
