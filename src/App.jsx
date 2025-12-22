b9cbec16d8f50e01c80b77058722ee828ee35dcb
Tuann <tuntrungnguyen@gmail.com>
2025-12-22 12:46:00 +0700

    123

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomePage from './Page/HomePage';
-import ChecklistPage from './Page/ChecklistPage';
-import AdminPage from './Page/AdminPage';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import ChecklistPage from './pages/ChecklistPage';
+import AdminPage from './pages/AdminPage';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        {/* 1. TRANG CHỦ (Mặc định) -> Giờ sẽ là trang ADMIN */}
+        <Route path="/" element={<AdminPage />} />
+
+        {/* 2. Trang làm bài (Giữ nguyên) */}
         <Route path="/checklist/:appId" element={<ChecklistPage />} />
+        
+        {/* 3. Trang Admin (Giữ thêm đường dẫn này để gõ /admin cũng vào được) */}
         <Route path="/admin" element={<AdminPage />} />
+
+        {/* 4. [TÙY CHỌN] Danh sách công việc cũ */}
+        {/* Nếu muốn xem danh sách việc cho nhân viên, giờ phải vào link /jobs */}
+        <Route path="/jobs" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
   );
 };
+
 export default App;
